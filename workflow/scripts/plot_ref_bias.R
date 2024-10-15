dced0c47e88449e794992049bf0f64dff9ac7077
Zachary Nolen <zachary.nolen@biol.lu.se>
2024-10-15 14:58:53 +0200

    Make IBS plot scale to population count

--- workflow/scripts/plot_ref_bias.R
+++ workflow/scripts/plot_ref_bias.R
@@ -8,6 +8,23 @@ ibstable_raw <- read.table(snakemake@input[["ibs"]], sep = "\t", header = TRUE)
 poplist <- read.table(snakemake@input[["pops"]], sep = "\t", header = TRUE)
 plotpre <- snakemake@params[["plotpre"]]
 
+# Some variables to automatically adjust the width of the plot based on the
+# number of categories on the x-axis. This is kind of based on the assumption
+# that 5ish fit comfortably at the default dimensions (7x7in), so we add 1/5 of
+# the width for every sample above 5.
+npops <- length(unique(poplist$population))
+if (npops <= 5) {
+  wpop <- 7
+} else {
+  wpop <- (7/5)*npops
+}
+ndp <- length(unique(poplist$depth))
+if (ndp <= 5) {
+  wdep <- 7
+} else {
+  wdep <- (7/5)*ndp
+}
+
 ibstable <- merge(ibstable_raw, poplist, by = "sample")
 
 ggplot(ibstable, aes(x = population, y = ibs.to.ref)) +
@@ -17,7 +34,7 @@ ggplot(ibstable, aes(x = population, y = ibs.to.ref)) +
   xlab("Population") +
   theme_classic()
 
-ggsave(paste0(plotpre, ".population.pdf"))
+ggsave(paste0(plotpre, ".population.pdf"), width = wpop, units = "in")
 
 ggplot(ibstable, aes(x = time, y = ibs.to.ref)) +
   geom_boxplot(outlier.shape = NA) +
@@ -35,7 +52,7 @@ ggplot(ibstable, aes(x = depth, y = ibs.to.ref)) +
   xlab("Depth Class") +
   theme_classic()
 
-ggsave(paste0(plotpre, ".depth.pdf"))
+ggsave(paste0(plotpre, ".depth.pdf"), width = wdep, units = "in")
 
 write.table(ibstable, file = snakemake@output[["table"]], quote=FALSE,
   sep = '\t', row.names = FALSE)

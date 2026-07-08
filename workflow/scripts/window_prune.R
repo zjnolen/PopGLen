e421cdc219ec438bf9bad48bff29a67943f1d995
Zachary J. Nolen <zachary.nolen@biol.lu.se>
2026-07-08 15:27:10 +0200

    Basic pruning in R based on bcftools +prune. For each position present in the beagle, the maximum r2 with position x kb upstream is estimated and all sites where this max r2 > r2thresh are dropped

---
+++ workflow/scripts/window_prune.R
@@ -0,0 +1,34 @@
+library(data.table)
+library(dplyr)
+
+# Reads in an LD table from ngsLD and prunes variants for which the maximum
+# r2 between upstream SNPs within a give distance exceeds a threshold.
+# Mimics behavior of bcftools +prune -w <window_size> -m <r2_thresh>, but for
+# ngsLD outputs. ngsLD must have been run with the max_kb_dist set to the size
+# of the window you want to use. Output is a list of sites in approximate
+# linkage equilibrium, but they won't be in order.
+
+ld_table <- snakemake@input[["ld"]]
+pos_file <- snakemake@input[["pos"]]
+out_list <- snakemake@output[["sites"]]
+r2_thresh <- snakemake@params[["r2"]]
+
+ld <- fread(ld_table, sep = "\t", header = FALSE)
+
+maxr2s <- df %>%
+  group_by(V2) %>%
+  summarize(maxr2 = max(V7))
+
+linked_snps <- maxr2s[maxr2s$maxr2 > r2_thresh, 1]
+
+pos <- fread(pos_file, sep = "\t", header = FALSE)
+
+pos <- paste(pos$V1, pos$V2, sep = ":")
+
+unlinked_snps <- pos[!pos %in% linked_snps]
+
+write.table(
+  unlinked_snps[, 2],
+  file = out_list,
+  quote = FALSE, row.names = FALSE, col.names = FALSE
+)
